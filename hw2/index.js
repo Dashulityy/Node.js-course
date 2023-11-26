ed3204d303206f40c6347e01d728c50b378d5eec
Darya Kolesnikova <Kdarias.07@gmail.com>
2023-11-26 21:46:52 +0500

    hw2

---
+++ hw2/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const LIMIT = 20;
+const DELAY = 1000;
+const PORT = 3000;
+
+let connections = [];
+app.get("/date", (req, res, next) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.setHeader("Transfer-Encoding", "chunked"); // https://en.wikipedia.org/wiki/Chunked_transfer_encoding
+    connections.push(res);
+});
+
+let tick = 0;
+setTimeout(function run(){
+    let data = Date();
+    console.log(data);
+    ++tick;
+    if(++tick > LIMIT){
+        connections.map(res => {
+            res.write("END\n");
+            res.end();
+        });
+        connections = [];
+        tick = 0;
+    }
+    connections.map((res, index) => {
+        res.write(`Hello ${index}! Date: ${data}.\n`);
+    });
+    setTimeout(run, DELAY);
+}, DELAY);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
